7b12c89bbe3aa77eed24b08b889e81e532ae6a5c
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2024-11-07 05:39:13 +0000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -10,12 +10,10 @@ SHOP_NAME = os.getenv('SHOP_NAME', 'cycle1-test')
 API_VERSION = os.getenv('API_VERSION', '2023-01')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 
-# New config for Celery
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
-
-# Redis config
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+# Redis and Celery config
+REDIS_URL = os.getenv('REDIS_URL', 'redis://default:cBVQaXcbrUZMvausFvgIvJQNwBjQcgNS@redis.railway.internal:6379')
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
 
 # API URLs
 SHOPIFY_API_URL = f"https://{SHOP_NAME}.myshopify.com/admin/api/{API_VERSION}/products.json"
