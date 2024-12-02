7f81cec4411aa6391c98b72d45e8388c9c9f4b27
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2024-12-02 06:15:37 +0200

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 from .auth import auth_blueprint
 from .products import products_blueprint
+from .main import main_blueprint
 
-# Register all blueprints
 def init_routes(app):
+    app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(products_blueprint)
