33441c7d6ab04fa16e37916dac63f2cea724ca9b
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-01-03 16:18:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -574,4 +574,4 @@ def index():
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
-            const productCount = document.getElementByI
+            
